4cb03b99d053e080a16eade9c7271cef188cd30b
sarambru <sarambiru@gmail.com>
2021-10-20 14:23:14 -0500

    API addition

---
+++ app/api.js
@@ -0,0 +1,43 @@
+const config = require('../app/config')
+const store = require('../app/store')
+const signUp = (formData) => {
+  return $.ajax({
+    url: `${config.apiUrl}/sign-up`,
+    method: 'POST',
+    data: formData
+  })
+}
+const signIn = (formData) => {
+  return $.ajax({
+    url: `${config.apiUrl}/sign-in`,
+    method: 'POST',
+    data: formData
+  })
+}
+
+const signOut = (formData) => {
+  return $.ajax({
+    url: `${config.apiUrl}/sign-out`,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+const newGame = (formData) => {
+  return $.ajax({
+    url: `${config.apiUrl}/games`,
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    },
+    data: {}
+  })
+}
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  newGame
+}
